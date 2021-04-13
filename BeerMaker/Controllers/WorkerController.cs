8c241e572016942046415fb86a760b7318390077
Mateusz Piwowarski <mateusz.piwowarski@euvic.pl>
2021-04-13 19:06:15 +0200

    empty project

---
+++ BeerMaker/Controllers/WorkerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeerMaker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BeerMaker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WorkerController : ControllerBase
+    {
+        private readonly ILogger<WorkerController> _logger;
+
+        public WorkerController(ILogger<WorkerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> GetItems()
+        {
+            return null;
+        }
+        public async Task<IActionResult> GetItem(Guid uuid)
+        {
+            return null;
+        }
+        public async Task<IActionResult> AddItem([FromBody] WorkerEntry item)
+        {
+            return null;
+        }
+        public async Task<IActionResult> UpdateItem(Guid uuid, [FromBody] WorkerEntry item)
+        {
+            return null;
+        }
+
+        public async Task<IActionResult> DeleteItem(Guid uuid)
+        {
+            return null;
+        }
+    }
+}
---
+++ BeerMaker/Entities/Worker.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeerMaker
+{
+	public class Worker
+	{
+		public int ID { get; set; }
+		public Guid Uuid { get; set; }
+		public string Email { get; set; }
+		public string HashedPassword { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Address { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		//public virtual ICollection<Statistics> Statistics { get; set; }
+	}
+}
